9c876392af35397dacc3bf642b177f2affec8044
Chris Garaffa <chris@chrisgaraffa.com>
2020-12-16 12:04:55 -0500

    Load config.yml

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,13 @@ module PocketHotline
     config.filter_parameters += [:password]
 
     config.assets.initialize_on_precompile = false
+
+    #local up local_env.yml if it exists.
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
