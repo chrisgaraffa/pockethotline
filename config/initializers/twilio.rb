64b1c0b888e047f0ab236c3642f3da8fd41d0264
Chris Garaffa <chris@chrisgaraffa.com>
2020-12-16 12:06:28 -0500

    Update twilio helper for v5 gem

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -2,5 +2,5 @@ if Rails.configuration.x.twilio.account_sid.present? && Rails.configuration.x.tw
   TWILIO = Twilio::REST::Client.new(
     Rails.configuration.x.twilio.account_sid,
     Rails.configuration.x.twilio.auth_token
-  ).account
+  )
 end
