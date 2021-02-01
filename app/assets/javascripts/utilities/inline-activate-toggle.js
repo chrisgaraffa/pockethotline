13610ad810654ca229097a309455ee961b17149f
Chris Garaffa <chris@chrisgaraffa.com>
2021-02-01 10:30:49 -0500

    No need for a container for active/inactive toggle

--- app/assets/javascripts/utilities/inline-activate-toggle.js
+++ app/assets/javascripts/utilities/inline-activate-toggle.js
@@ -1,18 +1,14 @@
 function initActivateTargets() {
-	const targets = document.querySelectorAll('.active-inactive-container');
+	const targets = document.querySelectorAll('.active-inactive-toggle');
 
-	targets.forEach(function(target) {
-		if (!testTargetForActiveInactiveInteraction(target)) { return; }
-
-		target.querySelectorAll('.active-inactive-toggle').forEach(function(el) {
-			if (!testElementForActiveInactiveInteraction(el)) { return; }
-
+	targets.forEach(function(el) {
+		if (!testTargetForActiveInactiveInteraction(el)) { return; }
 			const activeIcon = el.hasAttribute('data-activeicon') ? el.getAttribute('data-activeicon'): 'bi-star-fill';
 			const inactiveIcon = el.hasAttribute('data-inactiveicon') ? el.getAttribute('data-inactiveicon'): 'bi-star';
 
 			el.addEventListener('click', function(e) {
 				e.preventDefault();
-				var response = fetch(target.getAttribute('data-apipoint') + '/' + el.getAttribute('data-elementid') + '/toggle_active', {
+				var response = fetch(el.getAttribute('data-apipoint') + '/' + el.getAttribute('data-elementid') + '/toggle_active', {
 					method: 'PATCH',
 					cache: 'no-cache',
 				}).then(response => response.json())
@@ -30,18 +26,11 @@ function initActivateTargets() {
 				});
 				
 			});
-		});
 	});
 
 
 	function testTargetForActiveInactiveInteraction(el) {
-		var status = el.hasAttribute('data-apipoint');
-
-		return status;
-	}
-
-	function testElementForActiveInactiveInteraction(el) {
-		var status = el.hasAttribute('data-elementid');
+		var status = el.hasAttribute('data-apipoint') && el.hasAttribute('data-elementid');
 
 		return status;
 	}
